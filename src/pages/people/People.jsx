3ad1ce458474ccda898d3a06fb7fbd0fa492f612
suryacodess <helloosuryaprakash@gmail.com>
2023-08-25 13:12:59 +0530

    minor updates

--- src/pages/people/People.jsx
+++ src/pages/people/People.jsx
@@ -15,20 +15,25 @@ export default function People() {
       });
   };
 
-  console.log(popularPeople);
-
   useEffect(() => {
     getPeople();
   }, []);
 
   return (
-    <main className="w-full min-h-[100vh] mt-10">
+    <main className="w-full mt-10">
       <div className="max-w-[1200px] w-full mx-auto grid grid-cols-4 gap-10 ">
         {popularPeople?.map((people) => {
           return (
-            <div key={people.id} className="w-full flex justify-center items-center flex-col">
+            <div
+              key={people.id}
+              className="w-full flex justify-center items-center flex-col"
+            >
               <div>
-                <img className="w-full h-full" src={cast_img + people.profile_path} alt="" />
+                <img
+                  className="w-full h-full"
+                  src={cast_img + people.profile_path}
+                  alt=""
+                />
               </div>
               <div>
                 <h5 className="font-bold pt-5">{people?.name}</h5>
